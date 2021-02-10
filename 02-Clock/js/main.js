0d78cbe21edbe6668f516ac9bb1de824942c011a
akadiraslan <71260913+akadiraslan@users.noreply.github.com>
2021-02-10 17:46:44 +0300

    clock-02

---
+++ 02-Clock/js/main.js
@@ -0,0 +1,23 @@
+const secondHandDOM = document.querySelector('.second-hand');
+const minsHandDOM = document.querySelector('.min-hand');
+const hourHandDOM = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHandDOM.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHandDOM.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHandDOM.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
